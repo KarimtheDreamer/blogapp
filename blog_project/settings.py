302180e3e10d320801c73978f2d6ebabcb13efab
Faris <fariskarim2000@gmail.com>
2022-02-09 17:24:44 -0500

    heroku config files

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0_nof+=%jqwq(_uvsoopv8ggx47djy(-za9%c^&*r_@gl!uw^2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
